3cad639b66f51dc9c2567514a496c44ed5e29073
Дмитрий Лохов <DLokhov@me.com>
2020-06-02 11:49:09 +0900

    1

---
+++ lesson.py
@@ -0,0 +1,35 @@
+# проверка числа на простоту (простые числа - это те числа у которых делители единица и они сами);
+
+def is_prime(n):
+    a = 2
+    while num > a:
+        if num % a == 0:
+            break
+        a += 1
+    return a == n
+print(is_prime(96969))
+
+# выводит список всех делителей числа;
+def all_dividers(n):
+    result = []
+    a = 2
+    while a * a <= n:
+        if n % a == 0:
+            n //= a
+            result.append(a)
+        else:
+            a += 1
+    if  n > 1:
+        result.append(n)
+    return result
+print(all_dividers(64676476))
+
+# выводит самый большой простой делитель числа.
+def greatest_pr_div(n):
+    prime_num = all_dividers(n)
+    max_num = 0
+    for a in prime_num:
+        if a > max_num:
+            max_num = a
+    return(max_num)
+print(greatest_pr_div(4971049704))
